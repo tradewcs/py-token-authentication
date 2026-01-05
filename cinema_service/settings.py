736afca59e52e59e16aebbc3095bb204ff1fe313
Mike <tradewcs@gmail.com>
2026-01-05 17:38:45 +0200

    fixed typo

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -118,6 +118,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ),
 }
 
 AUTH_USER_MODEL = "user.User"
